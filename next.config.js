db111a2e0e61f83ec4be38252d293381bec379e9
Ben Marshall <ben.s.marshall@icloud.com>
2022-11-17 15:50:22 -0600

    working on deploy script

--- next.config.js
+++ next.config.js
@@ -5,28 +5,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    config.resolve.alias["@/components"] = path.resolve(
-      __dirname,
-      "./app/components"
-    );
-
-    config.resolve.alias["@/functions"] = path.resolve(
-      __dirname,
-      "./functions"
-    );
-
-    config.resolve.alias["@/lib"] = path.resolve(__dirname, "./lib");
-
-    config.resolve.alias["@/scss"] = path.resolve(__dirname, "./scss");
-
-    config.resolve.alias["@/app"] = path.resolve(__dirname, "./app");
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
