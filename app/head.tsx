cd444f8f72d242269dc6f57f720bd78ef18a64d2
Ben Marshall <ben.s.marshall@icloud.com>
2022-11-17 15:55:15 -0600

    working on deploy script

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,7 @@
 import getPostTypeStaticProps from "@/functions/wordpress/postTypes/getPostTypeStaticProps";
 
 // Import component dependencies
-import Meta from "@/app/components/molecules/Meta/Meta";
+import Meta from "@/components/molecules/Meta/Meta";
 
 export default async function Head() {
   // Retreive dynamic page data
