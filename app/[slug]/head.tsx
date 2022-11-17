60b1932e88499d39c8d80e3945c0c94cc2227fe5
Ben Marshall <ben.s.marshall@icloud.com>
2022-11-17 09:22:26 -0600

    fixes #5

--- app/[slug]/head.tsx
+++ app/[slug]/head.tsx
@@ -1,11 +1,5 @@
-// Import Next.js dependencies
-import { notFound } from "next/navigation";
-
 // Import WordPress dependencies
 import getPostTypeStaticProps from "@/functions/wordpress/postTypes/getPostTypeStaticProps";
-
-// Import library dependencies
-import parse from "html-react-parser";
 
 // Import component dependencies
 import Meta from "@/components/molecules/Meta/Meta";
@@ -13,13 +7,6 @@ import Meta from "@/components/molecules/Meta/Meta";
 export default async function Head({ params }: { params: { slug: string } }) {
   // Retreive dynamic page data
   let page = await getPostTypeStaticProps(params, "page");
-  if (page.notFound) {
-    page = await getPostTypeStaticProps(params, "post");
-  }
-
-  if (page.notFound) {
-    notFound();
-  }
 
   return <Meta seo={page?.props?.post?.seo} />;
 }
--- app/head.tsx
+++ app/head.tsx
@@ -1,6 +1,3 @@
-// Import Next.js dependencies
-import { notFound } from "next/navigation";
-
 // Import WordPress dependencies
 import getPostTypeStaticProps from "@/functions/wordpress/postTypes/getPostTypeStaticProps";
 
@@ -10,9 +7,5 @@ import Meta from "@/components/molecules/Meta/Meta";
 export default async function Head() {
   // Retreive dynamic page data
   let page = await getPostTypeStaticProps({ slug: "/" }, "page");
-  if (page.notFound) {
-    notFound();
-  }
-
   return <Meta seo={page?.props?.post?.seo} />;
 }
