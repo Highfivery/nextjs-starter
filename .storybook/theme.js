952a4ed25887fb2a1c97e08cfa308da408210d7b
Ben Marshall <ben.s.marshall@icloud.com>
2023-01-13 08:03:47 -0600

    build(docs): updating docs

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -8,5 +8,7 @@ import { create } from "@storybook/theming/create";
 export default create({
   base: "light",
   brandTitle: "Next.js Starter",
-  brandUrl: "",
+  brandUrl: "https://github.com/Highfivery/nextjs-starter",
+  brandImage:
+    "https://raw.githubusercontent.com/Highfivery/nextjs-starter/8d57589e0fcef4875944bb15ed16aa07fe49aebd/public/nextjs-starter.svg",
 });
