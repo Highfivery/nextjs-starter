cca337589b52eda414c656e0ef0a45693fbb600a
Ben Marshall <ben.s.marshall@icloud.com>
2023-04-07 20:00:50 -0500

    fix(build): removed submodule requirements & error with deprecated head file

--- app/head.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * Import internal component dependencies
- *
- */
-import Meta from "@/components/atomic-design/molecules/Meta/Meta";
-
-export default function Head() {
-  return <Meta seo={{}} />;
-}
